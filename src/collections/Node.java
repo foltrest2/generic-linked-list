92fa57b3ad28de8cfc2a82908e3012ae5e2b3de8
Kevin Mera <kamcak@hotmail.es>
2021-02-25 22:03:19 -0500

    Nodo listo

--- src/collections/Node.java
+++ src/collections/Node.java
@@ -1,5 +1,33 @@
 package collections;
 
-public class Node {
+public class Node <T1>{
 
+	private Node<?> nextNode;
+	private T1 data;
+	
+	
+	public Node(T1 data) {
+		this.data = data;
+	}
+
+
+
+	public Node<?> getNextNode() {
+		return nextNode;
+	}
+
+
+	public void setNextNode(Node<?> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+
+	public T1 getData() {
+		return data;
+	}
+
+
+	public void setData(T1 data) {
+		this.data = data;
+	}
 }
