06cd8d683a0daffd665331b27a6794eb4a469053
Julian Riascos <riascosjulian5@gmail.com>
2021-02-25 23:00:19 -0500

    Working on Linked List

--- src/collections/LinkedList.java
+++ src/collections/LinkedList.java
@@ -1,19 +1,48 @@
 package collections;
 
-public class LinkedList<T> implements GenericLinkedListMethods <T> {
+public class LinkedList<T> implements GenericLinkedListMethods <T>, Comparable<T> {
 
-	private Node first;
-	
+	private Node<T> first;
+
+	public LinkedList() {
+		first = null;
+	}
+
+	public Node<T> getFirstNode(){
+		return first;
+	}
+
 	@Override
 	public boolean addElement(T t) {
-		// TODO Auto-generated method stub
+		Node<T> newNode = new Node<T>(t);
+		if(first==null) {
+			first = newNode;
+		}else {
+			Node<T> current = first;
+			while(current.getNextNode()!=null) {
+				current = current.getNextNode();
+			}
+			current.setNextNode(newNode);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean searchElement(T t) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<?> n = null;
+		Node<?> current = first;
+		while(current!=null && n==null) {
+			if(t.equals(current.getData())) {
+				n = current;
+			}
+			current = current.getNextNode();
+		}
+		if (n != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	@Override
@@ -32,6 +61,19 @@ public class LinkedList<T> implements GenericLinkedListMethods <T> {
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public int compareTo(T t) {
+		int comp = 0;
+		if (((Comparable<T>) first.getData()).compareTo(t) < 0) {
+			comp = -1;
+		}
+		if (((Comparable<T>) first.getData()).compareTo(t) > 0) {
+			comp = -1;
+		}
+		return comp;
 	}
 
 }
